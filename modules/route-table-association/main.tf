03483ce03a5b1022fe6df97f0f7e5ba08855c654
abhishekchauhan98 <abhishek.chauhan1@tothenew.com>
2023-05-13 19:44:26 +0530

    route table association fix

--- modules/route-table-association/main.tf
+++ modules/route-table-association/main.tf
@@ -2,13 +2,13 @@ data "aws_subnet" "selected" {
   id = var.subnet_id
 }
 resource "aws_route_table_association" "route_table_association" {
-  count          = regex("*private-2", lookup(data.aws_subnet.selected.tags, "Name")) == "private-2" ? 0 : 1
+  count          = regex("private-2", lookup(data.aws_subnet.selected.tags, "Name")) == "private-2" ? 0 : 1
   route_table_id = var.route_table_id
   subnet_id      = var.subnet_id
 }
 
 resource "aws_route_table_association" "route_table_association2" {
-  count          = regex("*private-2", lookup(data.aws_subnet.selected.tags, "Name")) == "private-2" ? 1 : 0
+  count          = regex("private-2", lookup(data.aws_subnet.selected.tags, "Name")) == "private-2" ? 1 : 0
   route_table_id = var.route_table_id
   subnet_id      = var.subnet_id
 }
